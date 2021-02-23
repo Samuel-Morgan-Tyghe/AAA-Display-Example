9f47035ac6523883c105addeda1de0c474dcf52a
Samuel-Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-02-23 22:13:55 +0000

    Updates

---
+++ precache-manifest.24e8e1631cdfa8069a5fa65fc14c8525.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e9892573ccd64f715f5c0770dc35ce",
+    "url": "/Api-Account-App/index.html"
+  },
+  {
+    "revision": "b3c1c95b291c8773bf5b",
+    "url": "/Api-Account-App/static/css/main.c6aab55b.chunk.css"
+  },
+  {
+    "revision": "56179d1f9edd1a62f363",
+    "url": "/Api-Account-App/static/js/2.cc335743.chunk.js"
+  },
+  {
+    "revision": "c2e52282d033fca86fa91aac36af9388",
+    "url": "/Api-Account-App/static/js/2.cc335743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c1c95b291c8773bf5b",
+    "url": "/Api-Account-App/static/js/main.9386908e.chunk.js"
+  },
+  {
+    "revision": "5e7d3ac3bd8d9d0e3daa",
+    "url": "/Api-Account-App/static/js/runtime-main.354210cf.js"
+  },
+  {
+    "revision": "d9292581d261c628c56567de2f37ab0b",
+    "url": "/Api-Account-App/static/media/BackgroundImage3.d9292581.png"
+  },
+  {
+    "revision": "dd5d1f1330b45bbb50a288f581a78443",
+    "url": "/Api-Account-App/static/media/InnerBackground.dd5d1f13.png"
+  },
+  {
+    "revision": "7ef4cb1e8e2de898c844aecb50365ba4",
+    "url": "/Api-Account-App/static/media/background.7ef4cb1e.png"
+  },
+  {
+    "revision": "82658e188640c5d380d05cb5a74f2265",
+    "url": "/Api-Account-App/static/media/loading2.82658e18.gif"
+  }
+]);
